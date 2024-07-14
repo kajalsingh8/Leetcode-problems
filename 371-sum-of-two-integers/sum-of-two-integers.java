58d5d4ec429997bc4c5d7d4670253f351985538d
kajal singh <133157917+kajalsingh8@users.noreply.github.com>
2024-07-14 12:27:38 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (6.07%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.java
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -1,13 +1,13 @@
 class Solution {
     public int getSum(int a, int b) {
-        return add(a, b);//return the value of the a,b 
+        return add(a, b);
     }
 
     public int add(int a, int b) {
         while (b != 0) {
-            int carry = a & b;//calcuate using the AND 
-            a = a ^ b;//Calculate using the XOR 
-            b = carry << 1;//shift the carry for the next iteration 
+            int carry = a & b;
+            a = a ^ b; 
+            b = carry << 1; 
         }
         return a;//return the value of a 
     }
