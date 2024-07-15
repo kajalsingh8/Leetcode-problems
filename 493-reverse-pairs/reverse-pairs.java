51a5731735d6d24bd8e5e23ed7aae1ecef90c271
kajal singh <133157917+kajalsingh8@users.noreply.github.com>
2024-07-15 21:29:41 +0530

    Time: 51 ms (48.13%) | Memory: 55.6 MB (6.12%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.java
+++ 493-reverse-pairs/reverse-pairs.java
@@ -1,59 +1,33 @@
-class Solution {
+public class Solution {
+    int[] helper;
     public int reversePairs(int[] nums) {
-        /*int count=0;
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]>2*nums[j]) count++;
+        this.helper = new int[nums.length];
+        return mergeSort(nums, 0, nums.length-1);
+    }
+    private int mergeSort(int[] nums, int s, int e){
+        if(s>=e) return 0; 
+        int mid = s + (e-s)/2; 
+        int cnt = mergeSort(nums, s, mid) + mergeSort(nums, mid+1, e); 
+        for(int i = s, j = mid+1; i<=mid; i++){
+            while(j<=e && nums[i]/2.0 > nums[j]) j++; 
+            cnt += j-(mid+1); 
+        }
+        //Arrays.sort(nums, s, e+1); 
+        myMerge(nums, s, mid, e);
+        return cnt; 
+    }
+    
+    private void myMerge(int[] nums, int s, int mid, int e){
+        for(int i = s; i<=e; i++) helper[i] = nums[i];
+        int p1 = s;//pointer for left part
+        int p2 = mid+1;//pointer for rigth part
+        int i = s;//pointer for sorted array
+        while(p1<=mid || p2<=e){
+            if(p1>mid || (p2<=e && helper[p1] >= helper[p2])){
+                nums[i++] = helper[p2++];
+            }else{
+                nums[i++] = helper[p1++];
             }
         }
-        return count;
-        */
-        return mergeSort(nums,0,nums.length-1);
     }
-    public int mergeSort(int nums[],int low,int high){
-        int count = 0;
-        if (low < high) {
-            int mid = low + (high - low) / 2;
-            count += mergeSort(nums, low, mid);
-            count += mergeSort(nums, mid + 1, high);
-            count += merge(nums, low, mid, high);
-        }
-        return count;
-    }
-    public int merge(int []nums, int low,int mid,int high){
-        int[] leftArray = Arrays.copyOfRange(nums, low, mid + 1);
-        int[] rightArray = Arrays.copyOfRange(nums, mid + 1, high + 1);
-        
-        int i = 0, j = 0, k = low;
-        int count = 0;
-
-        while (i < leftArray.length && j < rightArray.length) {
-            if ((long) leftArray[i] > 2 * (long) rightArray[j]) {
-                count += leftArray.length - i;
-                j++;
-            } else {
-                i++;
-            }
-        }
-
-        i = 0;
-        j = 0;
-        while (i < leftArray.length && j < rightArray.length) {
-            if (leftArray[i] <= rightArray[j]) {
-                nums[k++] = leftArray[i++];
-            } else {
-                nums[k++] = rightArray[j++];
-            }
-        }
-
-        while (i < leftArray.length) {
-            nums[k++] = leftArray[i++];
-        }
-        while (j < rightArray.length) {
-            nums[k++] = rightArray[j++];
-        }
-
-        return count;
-    }
-
 }
